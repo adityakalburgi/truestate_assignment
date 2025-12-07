70ed4681eafae53ae8e87a127cc24416e9861610
adityakalburgi <adityakalburgi295@gmail.com>
2025-12-07 18:00:01 +0530

    first commit

---
+++ backend/src/routes/transactionRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getTransactions, getFilterOptions } from '../controllers/transactionController.js';
+
+const router = express.Router();
+
+router.get('/', getTransactions);
+router.get('/options', getFilterOptions);
+
+export default router;
